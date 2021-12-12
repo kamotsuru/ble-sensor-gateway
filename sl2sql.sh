21be80f40a62511386e878ae76c0ebaf17789cb0
kamotsuru <32958447+kamotsuru@users.noreply.github.com>
2021-12-12 14:06:40 +0900

    Update sl2sql.sh

--- sl2sql.sh
+++ sl2sql.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-
 #cd to this script located directory
 cd `dirname $0`
 
 #Usage
 #case $# in
 # 1 ) ;;
-# * ) echo "Usage: ${0##*/} syslog_file_name"; exit 1;;
+# * ) echo "Usage: ${0##*/} syslog_file"; exit 1;;
 #esac
 
 #sqlite3 db file name created by this script file name
@@ -18,9 +17,9 @@ cmd_sqlite3="/usr/bin/sqlite3"
 #create db if the above db file doesn't exist
 if [ ! -f ${dbfile} ]; then
   echo "${dbfile} doesn't exist!"
- ${cmd_sqlite3} ${dbfile} "CREATE TABLE sens_data (id INTEGER PRIMARY KEY AUTOINCREMENT, device VARCHAR(64), sensId INTEGER, current REAL, voltage REAL, timestamp TEXT, updated_at TEXT DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime')));"
-#else
-#  echo "${dbfile} exists!"
+  ${cmd_sqlite3} ${dbfile} "CREATE TABLE sens_data (id INTEGER PRIMARY KEY AUTOINCREMENT, device VARCHAR(64), sensId INTEGER, current REAL, busvoltage REAL, current_mA REAL, timestamp TEXT, updated_at TEXT DEFAULT (datetime(CURRENT_TIMESTAMP, 'localtime')));"  
+else
+  echo "${dbfile} exists!"
 fi
 
 #file=$1
@@ -30,9 +29,9 @@ file="/var/log/messages"
 ex_data=`${cmd_sqlite3} ${dbfile} "SELECT max(id), timestamp FROM sens_data;"` 
 ex_timestamp=`echo $ex_data | cut -d '|' -f 2`
 
+#read syslog file line by line
 flag=0
 
-#read syslog file line by line
 while read line
 do
     app=`echo ${line} | awk '{print $4}'`
@@ -42,19 +41,31 @@ do
     device=`echo ${line} | awk '{print $6}'`
     sensId=`echo ${line} | awk '{print $7}'`
     current=`echo ${line} | awk '{print $8}'`
-    voltage=`echo ${line} | awk '{print $9}'`
+    busvoltage=`echo ${line} | awk '{print $9}'`
+    current_mA=`echo ${line} | awk '{print $10}'`    
+    comp=`echo "$current > 128.0" | bc`
+    if [ $comp -eq 1 ]; then
+	current = `expr $current - 256.0`
+    fi
+    comp=`echo "$busvoltage > 128.0" | bc`    
+    if [ $comp -eq 1 ]; then
+       busvoltage = `expr $busvoltage - 256.0`     
+    fi
+    comp=`echo "$current_mA > 128.0" | bc`    
+    if [ $comp -eq 1 ]; then
+       current_mA = `expr $current_mA - 256.0`     
+    fi       
     timestamp="`echo ${line} | awk '{print $1}'` `echo ${line} | awk '{print $2}'` `echo ${line} | awk '{print $3}'`"
 #    echo $device   
 #    echo $sensId   
 #    echo $current   
-#    echo $voltage   
-#    echo $timestamp
-#flag if the latest sensor data found
-    if [ $flag -eq 1 ] || [ `date -d"${ex_timestamp}" +%s` -lt `date -d"${timestamp}" +%s` ]; then
+#    echo $busvoltage   
+#    echo $timestamp   
+    if [ `date -d"${ex_timestamp}" +%s` -lt `date -d"${timestamp}" +%s` ]; then
         flag=1
     fi
 #insert sensor data
     if [ $flag -eq 1 ]; then
-        ${cmd_sqlite3} ${dbfile} "INSERT INTO sens_data(device, sensId, current, voltage, timestamp) VALUES('${device}', '${sensId}', '${current}', '${voltage}', '${timestamp}');"
+        ${cmd_sqlite3} ${dbfile} "INSERT INTO sens_data(device, sensId, current, busvoltage, current_mA, timestamp) VALUES('${device}', '${sensId}', '${current}', '${busvoltage}', '${current_mA}', '${timestamp}');"	
     fi
 done < $file
